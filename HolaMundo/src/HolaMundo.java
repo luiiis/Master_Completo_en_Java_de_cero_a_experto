73e53e0d6d020455e6f89687dc598eddbb42c18a
luis <lgarciiaruiiz@gmail.com>
2025-05-02 18:56:46 -0600

    13 reglas para definir una variable

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,6 +2,21 @@ public class HolaMundo {
 //metodo principal con el emtodo main
 
 public static void main(String[] args) throws Exception {
-    System.out.println("Hello, Mundo!");
+    String saludar ="Hello, Mundo!";//tipo de dato cadena
+    System.out.println(saludar);
+    System.out.println(saludar.toUpperCase());//convertir todo a mayuscula
+
+    int numero =10;
+    int numero2 =5;
+
+    boolean valor =true;
+    if(valor){
+        System.out.println("numero = "+numero);
+        numero2 =10;
+    }
+    System.out.println("numero2 = "+numero2);
+
+    //version 10 en java var puede ser de cual quier tipo de dato
+    var numero3 = 15;
 }
 }
